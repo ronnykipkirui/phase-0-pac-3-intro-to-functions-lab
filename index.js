0d6c100ade8626e9dd7652193322478614e1a585
Ronny <ronnybet41@gmail.com>
2022-08-05 08:16:21 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log((string.toUpperCase()))
+}
+function logWhisper(string) {
+  console.log((string.toLowerCase()))
+}
+f
+function sayHiToHeadphonedRoommate(string){
+   if(string === string.toLowerCase()){
+       return "I can't hear you!"
+   }
+
+   if(string === string.toUpperCase()){
+       return "YES INDEED!"
+   }
+
+   if(string === "I love you, Grandma."){
+       return "I love you, too."
+   }
+
+   if(string === "Let's have dinner together!"){
+       return "I would love to!"
+   }
+}
+
+
+
+
+
+
+//   if (string === string.toLowerCase) {
+//    return"I can't hear you!"
+//   }
+
+//   else if (string === string.UpperCase) {
+//     console.log("YES INDEED!")
+
+//   }
+
+//   else if (string === "Let's have dinner together!") {
+//     console.log("I would love to!")
+
+//   }
+// }
